5a3dfe8c53e7e7ae32d698365eb3761435b0baba
soywiz <soywiz@gmail.com>
2013-06-01 02:22:54 +0200

    - First commit

---
+++ CSharpCpu/CSharpCpu/Cpus/InstructionFlags.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpCpu.Cpus
+{
+	public class InstructionFlags
+	{
+		public int DecodeSize;
+		public bool FixedSize;
+	}
+}
