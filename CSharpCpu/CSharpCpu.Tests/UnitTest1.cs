8f6833903a94bc8285be73f3b52649b77c63aede
soywiz <soywiz@gmail.com>
2013-06-02 02:06:53 +0200

    - Implemented SwitchGenerator

--- CSharpCpu/CSharpCpu.Tests/UnitTest1.cs
+++ CSharpCpu/CSharpCpu.Tests/UnitTest1.cs
@@ -1,25 +1,56 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using CSharpCpu.Decoder;
 using SafeILGenerator.Ast.Generators;
+using SafeILGenerator.Ast.Nodes;
+using SafeILGenerator;
+using System.Reflection.Emit;
+using SafeILGenerator.Ast.Optimizers;
+using System.Collections.Generic;
 
 namespace CSharpCpu.Tests
 {
 	[TestClass]
 	public class UnitTest1
 	{
+		static DecoderReference[] Table = new[] {
+			new DecoderReference() { Name = "test1", Mask = new[] { (uint)0xFF000000 }, Data = new[] { (uint)0x12000000 } },
+			new DecoderReference() { Name = "test2", Mask = new[] { (uint)0xFF000000 }, Data = new[] { (uint)0x20000000 } },
+			new DecoderReference() { Name = "test3", Mask = new[] { (uint)0xFF00000F }, Data = new[] { (uint)0x33000000 } },
+			new DecoderReference() { Name = "test4", Mask = new[] { (uint)0xFF00000F }, Data = new[] { (uint)0x33000001 } },
+			new DecoderReference() { Name = "test5", Mask = new[] { (uint)0xFF0000FF, (uint)0xFF000000 }, Data = new[] { (uint)0x33000042, (uint)0x01000000 } },
+		};
+
 		[TestMethod]
-		public void TestMethod1()
+		public void TestSwitch()
 		{
-			var SwitchGenerator = new SwitchGenerator();
 			var GeneratorCSharp = new GeneratorCSharp();
-			Console.WriteLine(GeneratorCSharp.GenerateString<GeneratorCSharp>(SwitchGenerator.GenerateSwitch(new[] {
-				new DecoderReference() { Mask = new[] { (uint)0xFF000000 }, Data = new[] { (uint)0x12000000 } },
-				new DecoderReference() { Mask = new[] { (uint)0xFF000000 }, Data = new[] { (uint)0x20000000 } },
-			})));
+
+			const string DefaultValue = "DEFAULT!";
+			
+			var SwitchTree = SwitchGenerator.GenerateSwitch(Table, (DecoderReference) =>
+			{
+				if (DecoderReference == null) return new AstNodeExprImm(DefaultValue);
+				return new AstNodeExprImm(DecoderReference.Name);
+			});
+			SwitchTree = (AstNodeStm)(new AstOptimizer().Optimize(SwitchTree));
+			var StringString = GeneratorCSharp.GenerateString<GeneratorCSharp>(SwitchTree);
+			var Func = GeneratorIL.GenerateDelegate<GeneratorIL, Func<Func<uint>, String>>("Decoder", SwitchTree);
+
+			Func<uint[], string> Decode = (Data) => {
+				var Reader = new Queue<uint>(Data);
+				return Func(() => Reader.Dequeue());
+			};
+
+			for (int n = 0; n < 5; n++) {
+				Assert.AreEqual(Table[n].Name, Decode(Table[n].Data));
+			}
+			Assert.AreEqual(DefaultValue, Decode(new uint[] { 0 }));
 		}
 
 		class DecoderReference : IDecoderReference {
+			public string Name;
 			public uint[] Data;
 			public uint[] Mask;
 
--- CSharpCpu/CSharpCpu/Decoder/SwitchGenerator.cs
+++ CSharpCpu/CSharpCpu/Decoder/SwitchGenerator.cs
@@ -1,32 +1,110 @@
-﻿using SafeILGenerator.Ast.Nodes;
+﻿using SafeILGenerator.Ast;
+using SafeILGenerator.Ast.Nodes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CSharpCpu.Decoder
 {
-	public class SwitchGenerator
+	sealed public class SwitchGenerator
 	{
-		private AstNodeExpr Process(IDecoderReference DecoderReference)
+		static public AstNodeStm GenerateSwitch<TDecoderReference>(IEnumerable<TDecoderReference> AllItems, Func<TDecoderReference, AstNodeExpr> Process, uint BaseMask = 0xFFFFFFFF) where TDecoderReference : IDecoderReference
 		{
-			return null;
+			return new SwitchGeneratorInternal<TDecoderReference>() {
+				Local = AstLocal.Create(typeof(uint), "CurrentWord"),
+				ReaderArgument = new AstArgument(0, typeof(Func<uint>), "Read"),
+				AllItems = AllItems,
+				Process = Process,
+			}.GenerateSwitch(BaseMask);
 		}
 
-		public AstNodeStm GenerateSwitch(IEnumerable<IDecoderReference> Items, uint BaseMask = 0xFFFFFFFF, int ReferenceIndex = 0)
+		sealed internal class SwitchGeneratorInternal<TDecoderReference> where TDecoderReference : IDecoderReference
 		{
-			Items = Items.Where(Item => Item.Mask.Length >= ReferenceIndex);
+			internal AstLocal Local;
+			internal AstArgument ReaderArgument;
+			internal Func<TDecoderReference, AstNodeExpr> Process;
+			internal IEnumerable<TDecoderReference> AllItems;
 
-			var CommonMask = Items
-				.Aggregate(BaseMask, (CurrentMask, Item) => CurrentMask & Item.Mask[ReferenceIndex])
-			;
+			internal AstNodeStm GenerateSwitch(uint BaseMask = 0xFFFFFFFF)
+			{
 
-			
-			return new AstNodeStmSwitch(
-				(new AstNodeExprImm(0)) & CommonMask,
-				Items.Select((Item) => new AstNodeCase(Item.Data[ReferenceIndex] & CommonMask, new AstNodeStmReturn(Process(Item)))).ToArray()
-			);
+				return new AstNodeStmContainer(
+					new AstNodeStmAssign(new AstNodeExprLocal(Local), new AstNodeExprCallDelegate(new AstNodeExprArgument(ReaderArgument))),
+					_GenerateSwitch(BaseMask, AllItems, 0, 0),
+					new AstNodeStmReturn(Process(default(TDecoderReference)))
+				);
+			}
+
+			private uint GetCommonMask(uint BaseMask, IEnumerable<TDecoderReference> Items, int ReferenceIndex)
+			{
+				return Items.Aggregate(BaseMask, (CurrentMask, Item) => CurrentMask & Item.Mask[ReferenceIndex]);
+			}
+
+			public int GetShiftRightMask(uint Mask)
+			{
+				int Count = 0;
+				while (Mask != 0 && ((Mask & 1) == 0)) { Mask >>= 1; Count++; }
+				return Count;
+			}
+
+			private AstNodeStm _GenerateSwitch(uint BaseMask, IEnumerable<TDecoderReference> Items, int ReferenceIndex, int NestLevel)
+			{
+				if (Items == null) Items = AllItems;
+				if (NestLevel > 10) throw(new Exception("Too much nesting. Probably an error."));
+
+				//Console.WriteLine("------------------------");
+
+				Items = Items.Where(Item => Item.Mask.Length > ReferenceIndex);
+
+				var CommonMask = GetCommonMask(BaseMask, Items, ReferenceIndex);
+				var CommonShift = GetShiftRightMask(CommonMask);
+
+				//Console.WriteLine("BaseMask: 0x{0:X8}", BaseMask);
+				//Console.WriteLine("CommonMask: 0x{0:X8} | 0x{1:X8}", CommonMask, ~CommonMask);
+
+				var Cases = new List<AstNodeCase>();
+
+				foreach (var Group in Items.GroupBy(Item => Item.Data[ReferenceIndex] & CommonMask))
+				{
+					AstNodeStm CaseBody;
+					var GroupMask = GetCommonMask(BaseMask, Group, ReferenceIndex);
+
+					//Console.WriteLine("  GroupMask: 0x{0:X8}, 0x{1:X8}", GroupMask, GroupMask & ~CommonMask);
+
+					if ((Group.Count() == 1) && ((GroupMask & ~CommonMask) == 0))
+					{
+						// Leaf.
+						if (ReferenceIndex < Group.First().Data.Length - 1)
+						{
+							//new AstNodeExprLocal(
+							CaseBody = new AstNodeStmContainer(
+								new AstNodeStmAssign(new AstNodeExprLocal(Local), new AstNodeExprCallDelegate(new AstNodeExprArgument(ReaderArgument))),
+								_GenerateSwitch(0xFFFFFFFF, null, ReferenceIndex + 1, NestLevel + 1)
+							);
+						}
+						else
+						{
+							CaseBody = new AstNodeStmReturn(Process(Group.First()));
+						}
+					}
+					else
+					{
+						CaseBody = _GenerateSwitch(BaseMask & ~CommonMask, Group, ReferenceIndex, NestLevel + 1);
+						//CaseBody = new AstNodeStmReturn(null);
+					}
+
+					Cases.Add(new AstNodeCase(((Group.First().Data[ReferenceIndex] >> CommonShift) & (CommonMask >> CommonShift)), CaseBody));
+				}
+
+				return new AstNodeStmSwitch(
+					new AstNodeExprBinop((new AstNodeExprLocal(Local)), ">>", CommonShift) & (CommonMask >> CommonShift),
+					Cases,
+					new AstNodeCaseDefault(new AstNodeStmReturn(Process(default(TDecoderReference))))
+				);
+			}
 		}
 	}
 }
